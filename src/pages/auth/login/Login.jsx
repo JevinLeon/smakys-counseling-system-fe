a809276cef4f862c975d8be44c08590a25804469
JevinLeon <jevinleon@gmail.com>
2024-05-27 00:09:29 +0700

    deploy

--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -10,16 +10,6 @@ const LoginPage = () => {
           <div className="relative z-20 flex items-center text-lg font-medium">
             SMA Katolik Yos Sudarso
           </div>
-          <div className="relative z-20 mt-auto">
-            <blockquote className="space-y-2">
-              <p className="text-lg">
-                &ldquo;This library has saved me countless hours of work and
-                helped me deliver stunning designs to my clients faster than
-                ever before.&rdquo;
-              </p>
-              <footer className="text-sm">Sofia Davis</footer>
-            </blockquote>
-          </div>
         </div>
         <div className="lg:p-8">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
